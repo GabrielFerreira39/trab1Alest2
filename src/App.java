cefe782181b31c9c23e925ec3508cee7e765548c
GabrielFerreira39 <gabrielmferreira11@gmail.com>
2023-04-12 00:55:51 -0300

    finish

--- src/App.java
+++ src/App.java
@@ -4,32 +4,54 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         List<Macaquinho> macaquinhos = new ArrayList<>();
-        Scanner sc = new Scanner(new File("src/teste.txt"));
-        ArrayList<String> texto = new ArrayList<>();
-        while (sc.hasNextLine()) {
-            texto.add(sc.nextLine());
-        }
+        Scanner sc = new Scanner(new File("src/1000macacos.txt"));
 
-        String linhaUm = texto.get(0);
+        String linhaUm = sc.nextLine();
 
         String[] arrayLinhaUm = (linhaUm.split(" "));
         int turnos = Integer.parseInt(arrayLinhaUm[1]);
 
-        for (int i = 1; i < texto.size(); i++) {
-            String linhaString = texto.get(i);
-            List<String> macacoDados = Arrays.asList(linhaString.split(" "));
-            int macacoId = Integer.parseInt(macacoDados.get(1));
-            int amigoPar = Integer.parseInt(macacoDados.get(4));
-            int amigoImpar = Integer.parseInt(macacoDados.get(7));
-            int qtdCocos = Integer.parseInt(macacoDados.get(9));
+        while (sc.hasNextLine()) {
+            String linhaString = (sc.nextLine());
+            String[] macacoDados = (linhaString.split(" "));
+            int macacoId = Integer.parseInt(macacoDados[1]);
+            int amigoPar = Integer.parseInt(macacoDados[4]);
+            int amigoImpar = Integer.parseInt(macacoDados[7]);
+            int qtdCocos = Integer.parseInt(macacoDados[9]);
 
-            Macaquinho macaquinho = new Macaquinho(macacoId, amigoPar, amigoImpar, qtdCocos);
+            int cocosPares = 0;
+            int cocosImpares = 0;
+            for (int j = 0; j < qtdCocos; j++) {
+                int pedras = Integer.parseInt(macacoDados[j + 11]);
+                if (pedras % 2 == 0) {
+                    cocosPares++;
+                } else {
+                    cocosImpares++;
+                }
+            }
+
+            Macaquinho macaquinho = new Macaquinho(macacoId, amigoPar, amigoImpar, cocosPares, cocosImpares);
             macaquinhos.add(macaquinho);
 
         }
 
-        // System.out.println(Macaquinho.toString());
+        for (int i = 0; i < turnos; i++) {
+            for (Macaquinho macaquinho : macaquinhos) {
+                int amigoPar = macaquinho.getAmigoPar();
+                int amigoImpar = macaquinho.getAmigoImpar();
+                macaquinho.trocaDeCocos(macaquinhos.get(amigoPar), macaquinhos.get(amigoImpar));
+            }
+        }
+        Macaquinho ganhador = macaquinhos.get(0);
 
+        for (Macaquinho macaquinho : macaquinhos) {
+            if (macaquinho.getQtdCocos() > ganhador.getQtdCocos()) {
+                ganhador = macaquinho;
+            }
+
+        }
+
+        System.out.println(ganhador);
     }
 
 }
--- src/Macaquinho.java
+++ src/Macaquinho.java
@@ -1,53 +1,60 @@
 public class Macaquinho {
     private int id;
-    private int qtdCocos;
     private int amigoPar;
     private int amigoImpar;
+    private int cocosPares;
+    private int cocosImpares;
 
-    public Macaquinho(int id, int qtdCocos, int amigoPar, int amigoImpar) {
+    public Macaquinho(int id, int amigoPar, int amigoImpar, int cocosPares, int cocosImpares) {
         this.id = id;
-        this.qtdCocos = qtdCocos;
         this.amigoPar = amigoPar;
         this.amigoImpar = amigoImpar;
+        this.cocosPares = cocosPares;
+        this.cocosImpares = cocosImpares;
+    }
+
+    public void trocaDeCocos(Macaquinho amigoPar, Macaquinho amigoImpar) {
+        amigoPar.recebeCocosPares(cocosPares);
+        amigoImpar.recebeCocosImpares(cocosImpares);
+        this.cocosPares = 0;
+        this.cocosImpares = 0;
+    }
+
+    public void recebeCocosPares(int cocosPares) {
+        this.cocosPares += cocosPares;
+    }
+
+    public void recebeCocosImpares(int cocosImpares) {
+        this.cocosImpares += cocosImpares;
     }
 
     public int getId() {
         return this.id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public int getQtdCocos() {
-        return this.qtdCocos;
-    }
-
-    public void setQtdCocos(int qtdCocos) {
-        this.qtdCocos = qtdCocos;
+        return this.cocosPares + this.cocosImpares;
     }
 
     public int getAmigoPar() {
         return this.amigoPar;
     }
 
-    public void setAmigoPar(int amigoPar) {
-        this.amigoPar = amigoPar;
-    }
-
     public int getAmigoImpar() {
         return this.amigoImpar;
     }
 
-    public void setAmigoImpar(int amigoImpar) {
-        this.amigoImpar = amigoImpar;
+    public int getCocosPares() {
+        return cocosPares;
     }
 
-    // @Override
-    // public static String toString() {
-    // return "Macaquinho " + id +
-    // " amigoPar -> " + amigoPar +
-    // " amigoImpar -> " + amigoImpar +
-    // " quantidade de cocos -> " + qtdCocos;
-    // }
+    public int getCocosImpares() {
+        return cocosImpares;
+    }
+
+    @Override
+    public String toString() {
+        return "Macaquinho vencedor é o número " + id +
+                " e sua quantidade de cocos foi " + getQtdCocos();
+    }
 }
